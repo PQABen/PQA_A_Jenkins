719aed63c3f6a43a8fd22ac0a9dca4b881292ab1
PQABen <benoit.poudrier@pqa.ca>
2018-03-29 12:32:23 -0300

    New message

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello PQA Academy World!");
             Console.ReadLine();
         }
     }
